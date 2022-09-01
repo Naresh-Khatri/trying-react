6eeca732aae029e0f0838f22e06c3ca08734f4b5
naresh-khatri <naresh.khatri2345@gmail.com>
2022-09-01 15:21:39 +0530

    remove warnings

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -134,15 +134,15 @@ export default function Footer() {
       </div>
       <div className="bg-gray-800 bg-opacity-75">
         <div className="container px-5 py-6 mx-auto flex items-center sm:flex-row flex-col">
-          <a className="flex title-font font-medium items-center md:justify-start justify-center text-white">
-            <img className="w-12" src={logoImg} />
+          <a href='/' className="flex title-font font-medium items-center md:justify-start justify-center text-white">
+            <img alt="logo" className="w-12" src={logoImg} />
             <span className="ml-3 text-xl">Example Pvt Ltd.</span>
           </a>
           <p className="text-sm text-gray-400 sm:ml-6 sm:mt-0 mt-4">
             © 2022 Example Pvt Ltd.
           </p>
           <span className="inline-flex sm:ml-auto sm:mt-0 mt-4 justify-center sm:justify-start">
-            <a className="text-gray-400">
+            <a href="/" className="text-gray-400">
               <svg
                 fill="currentColor"
                 strokeLinecap="round"
@@ -154,7 +154,7 @@ export default function Footer() {
                 <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
               </svg>
             </a>
-            <a className="ml-3 text-gray-400">
+            <a href="/" className="ml-3 text-gray-400">
               <svg
                 fill="currentColor"
                 strokeLinecap="round"
@@ -166,7 +166,7 @@ export default function Footer() {
                 <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"></path>
               </svg>
             </a>
-            <a className="ml-3 text-gray-400">
+            <a href="/" className="ml-3 text-gray-400">
               <svg
                 fill="none"
                 stroke="currentColor"
@@ -180,7 +180,7 @@ export default function Footer() {
                 <path d="M16 11.37A4 4 0 1112.63 8 4 4 0 0116 11.37zm1.5-4.87h.01"></path>
               </svg>
             </a>
-            <a className="ml-3 text-gray-400">
+            <a href="/" className="ml-3 text-gray-400">
               <svg
                 fill="currentColor"
                 stroke="currentColor"
--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -29,7 +29,7 @@ export default function Home() {
                 <h2 className="title-font font-semibold text-white tracking-widest text-xs">
                   EMAIL
                 </h2>
-                <a className="text-indigo-400 leading-relaxed">
+                <a href="mailto: example@email.com" className="text-indigo-400 leading-relaxed">
                   example@email.com
                 </a>
                 <h2 className="title-font font-semibold text-white tracking-widest text-xs mt-4">
